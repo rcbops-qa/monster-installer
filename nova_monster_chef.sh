bb52198cb75970d1e64a01775bd4c884e216faad
James Courtois <jcourtois@thoughtworks.com>
2014-06-04 12:11:21 -0500

    james | prefer pip

--- nova_monster_chef.sh
+++ nova_monster_chef.sh
@@ -152,7 +152,7 @@ sshpass -p $PASSWORD ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyCheckin
   source ~/monster/.venv/bin/activate;
   pip install -r ~/monster/requirements.txt > /dev/null;
   cd ~/monster;
-  python setup.py install;
+  pip install -e . > /dev/null;
   printf \"rackspace:\n  user: $node_user\n  api_key: $node_api\n  auth_url: https://identity.api.rackspacecloud.com/v2.0/\n  region: $REGION2\n  plugin: rackspace\ncloudfiles:\n  user: $node_user\n  password: $node_password\nrazor:\n    ip: $razor_ip\ndefault_pass: $default_pass\" > secret.yaml"
 rm nova.error
 printf "Monster has been installed and configured on $chef_name!\nIP: $IP\nPassword: $PASSWORD\n"
